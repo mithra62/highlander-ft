4f8e056e255a75db3ca163213a01c1949b1f56fa
Eric Lamb <eric@mithra62.com>
2023-12-29 14:30:53 -0700

    adds install method form

--- system/user/addons/highlander_ft/addon.setup.php
+++ system/user/addons/highlander_ft/addon.setup.php
@@ -1,12 +1,15 @@
 <?php
 
+const HIGHLANDER_FIELDTYPE_VERSION = '1.0.0';
+
+
 return [
     'name'              => 'highlander_ft',
     'description'       => 'highlander_ft description',
-    'version'           => '1.0.0',
+    'version'           => HIGHLANDER_FIELDTYPE_VERSION,
     'author'            => 'mithra62',
     'author_url'        => 'fdsa',
-    'namespace'         => 'Mithra\HighlanderFt',
+    'namespace'         => 'Mithra62\HighlanderFt',
     'settings_exist'    => false,
     'fieldtypes'        => [
         'highlander_ft' => [
--- system/user/addons/highlander_ft/ft.highlander_ft.php
+++ system/user/addons/highlander_ft/ft.highlander_ft.php
@@ -1,19 +1,41 @@
 <?php
 
-if (! defined('BASEPATH')) {
+if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
 class Highlander_ft_ft extends EE_Fieldtype
 {
-    public $info = array(
-        'name'      => 'Highlander_ft',
-        'version'   => '1.0.0',
-    );
+    public $info = [
+        'name' => 'Highlander_ft',
+        'version' => HIGHLANDER_FIELDTYPE_VERSION,
+    ];
+
+    /**
+     * The available "types" of fields the FT will make available
+     * @var array
+     */
+    protected array $field_types = [
+        'input' => 'Input',
+        'textarea' => 'Textarea',
+        'password' => 'Password',
+    ];
+
+    /**
+     * @var array|string[]
+     */
+    protected array $unique_to = [
+        'all' => 'All',
+        'Entry\'s Channel',
+    ];
 
     public function install()
     {
-        return [];
+        return [
+            'display_field_type' => 'input',
+            'field_max_length' => '128',
+            'unique_to' => 'all',
+        ];
     }
 
     public function display_global_settings()
@@ -32,6 +54,50 @@ class Highlander_ft_ft extends EE_Fieldtype
 
     public function display_settings($data)
     {
+        $selected = element('display_field_type', $data);
+        $field_max_length = !empty($data['field_max_length']) ? $data['field_max_length'] : 128 ;
+        $settings = [
+            [
+                'title' => 'display_type',
+                'desc' => 'display_type_instructions',
+                'fields' => [
+                    'display_field_type' => [
+                        'name' => 'display_field_type',
+                        'type' => 'select',
+                        'choices' => $this->field_types,
+                        'value' => $selected,
+                    ],
+                ],
+            ],
+            [
+                'title' => 'field_max_length',
+                'fields' => [
+                    'field_max_length' => [
+                        'name' => 'field_max_length',
+                        'type' => 'text',
+                        'value' => $field_max_length,
+                    ],
+                ],
+            ],
+            [
+                'title' => 'unique_to',
+                'fields' => [
+                    'unique_to' => [
+                        'name' => 'unique_to',
+                        'type' => 'select',
+                        'value' => $field_max_length,
+                        'choices' => $this->unique_to,
+                    ],
+                ],
+            ],
+        ];
+
+        return ['field_options_highlander_fieldtype' => [
+            'label' => 'field_options',
+            'group' => 'highlander_fieldtype',
+            'settings' => $settings,
+        ],
+        ];
     }
 
     public function save_settings($data)
@@ -41,15 +107,20 @@ class Highlander_ft_ft extends EE_Fieldtype
 
     public function display_field($data)
     {
-        return form_input(array(
-            'name'  => $this->field_name,
-            'id'    => $this->field_id,
-            'value' => $data
-        ));
+        return form_input([
+            'name' => $this->field_name,
+            'id' => $this->field_id,
+            'value' => $data,
+        ]);
     }
 
-    public function replace_tag($data, $params = array(), $tagdata = false)
+    public function replace_tag($data, $params = [], $tagdata = false)
     {
         return 'Magic!';
+    }
+
+    public function validate($data)
+    {
+        return "you failed";
     }
 }
